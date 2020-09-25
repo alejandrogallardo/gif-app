c49d2af9e884db68438fa88d157bad96f06928a9
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-09-25 15:21:53 -0600

    se creo la carpeta docs

---
+++ docs/precache-manifest.9cfe37a4d32086f237306c646b1c6c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9974e42e827c3aafb9bfbe826d985c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d7a8cd24748085afea",
+    "url": "/static/css/main.45ad127e.chunk.css"
+  },
+  {
+    "revision": "f4f94b6f63ef6d271e4b",
+    "url": "/static/js/2.527f945a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527f945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d7a8cd24748085afea",
+    "url": "/static/js/main.860a6e5a.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
